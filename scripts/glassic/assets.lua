cbaf065376356a9758bc939a1d1001696f7f4e09
秋一 <civi@rcg.fun>
2021-06-23 22:04:01 +0800

    init ga_inventoryimages

--- scripts/glassic/assets.lua
+++ scripts/glassic/assets.lua
@@ -6,13 +6,4 @@ PrefabFiles = {
     "moonglasspickaxe",
 }
 
--- GlassicAPI.RegisterItemAtlas("ga_inventoryimages")
-local RegisterItemAtlas = GlassicAPI.RegisterItemAtlas
-RegisterItemAtlas("inventoryimages/glassiccutter", Assets)
-RegisterItemAtlas("inventoryimages/moonglasshammer", Assets)
-RegisterItemAtlas("inventoryimages/moonglasspickaxe", Assets)
-RegisterItemAtlas("inventoryimages/goldenaxe_victorian", Assets)
-RegisterItemAtlas("inventoryimages/moonglassaxe_northern", Assets)
-RegisterItemAtlas("inventoryimages/moonglassaxe_victorian", Assets)
-RegisterItemAtlas("inventoryimages/moonglasshammer_forge", Assets)
-RegisterItemAtlas("inventoryimages/moonglasspickaxe_northern", Assets)
+GlassicAPI.RegisterItemAtlas("ga_inventoryimages", Assets)
