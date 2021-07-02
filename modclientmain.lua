bfffb0ff6da4de96f9562a870da1e671ab058641
Tony <18810595035@163.com>
2021-07-03 01:30:22 +0800

    update modclientmain

--- modclientmain.lua
+++ modclientmain.lua
@@ -1,8 +1,13 @@
 if GLOBAL.TheNet:GetIsClient() or GLOBAL.TheNet:GetIsServer() then return end
 
-modimport("modmain")
+modimport("modmain") -- For API functions & mod env
 
-GlassicAPI.SkinHandler.SetRarity("Glassic", 0.1, { 40 / 255, 150 / 255, 128 / 255, 1 }, "reward")
+-- Glassic rarity part below
+
+Assets = {
+    Asset("ANIM", "anim/glassic_rarities.zip"),
+}
+GlassicAPI.SkinHandler.SetRarity("Glassic", 0.1, { 40 / 255, 150 / 255, 128 / 255, 1 }, "glassic", "glassic_rarities")
 
 local STRINGS = GLOBAL.STRINGS
 if table.contains({"zh", "chs", "cht"}, GLOBAL.LanguageTranslator.defaultlang) then
