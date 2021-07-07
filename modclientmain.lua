c64f176c3c67595ecdc342fb3ac8eac890357916
秋一 <civi@rcg.fun>
2021-07-07 15:42:56 +0800

    unknown modify.

--- modclientmain.lua
+++ modclientmain.lua
@@ -2,16 +2,13 @@ if GLOBAL.TheNet:GetIsClient() or GLOBAL.TheNet:GetIsServer() then return end
 
 modimport("modmain") -- For API functions & mod env
 
--- Glassic rarity part below
-
 Assets = {
     Asset("ANIM", "anim/glassic_rarities.zip"),
 }
 GlassicAPI.SkinHandler.SetRarity("Glassic", 0.1, { 40 / 255, 150 / 255, 128 / 255, 1 }, "glassic", "glassic_rarities")
 
-local STRINGS = GLOBAL.STRINGS
 if table.contains({"zh", "chs", "cht"}, GLOBAL.LanguageTranslator.defaultlang) then
-    STRINGS.UI.RARITY.Glassic = "玻璃制品"
+    GLOBAL.STRINGS.UI.RARITY.Glassic = "玻璃制品"
 else
-    STRINGS.UI.RARITY.Glassic = "Glassic"
+    GLOBAL.STRINGS.UI.RARITY.Glassic = "Glassic"
 end
