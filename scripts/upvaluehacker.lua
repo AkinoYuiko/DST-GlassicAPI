75adf66acc8e95592f35c3638a6994a9b35abcb3
Tony <18810595035@163.com>
2021-07-20 09:07:54 +0800

    Update upvaluehacker

--- scripts/upvaluehacker.lua
+++ scripts/upvaluehacker.lua
@@ -1,30 +1,35 @@
 --Tool designed by Rezecib.
 --Personal Changed by Tony.
 
-local function GetUpvalueHelper(fn, name)
+local function GetUpvalueHelper(entry_fn, entry_name)
     local i = 1
-    while debug.getupvalue(fn, i) and debug.getupvalue(fn, i) ~= name do
+    while true do
+        local name, value = debug.getupvalue(entry_fn, i)
+        if name == entry_name then
+            return value, i
+        elseif name == nil then
+            return nil
+        end
         i = i + 1
     end
-    local name, value = debug.getupvalue(fn, i)
-    return value, i
 end
- 
+
 local function GetUpvalue(fn, path)
-    local prv, i, prv_var = nil, nil, nil
+    local prv, i = nil, nil
     for var in path:gmatch("[^%.]+") do
         prv = fn
         fn, i = GetUpvalueHelper(fn, var)
+        if fn == nil then return end
     end
     return fn, i, prv
 end
- 
+
 local function SetUpvalue(start_fn, path, new_fn)
-    local _fn, _fn_i, scope_fn = GetUpvalue(start_fn, path)
-    if not _fn then print("Didn't find "..path.." from", start_fn) return end
-    debug.setupvalue(scope_fn, _fn_i, new_fn)
+    local fn, fn_i, scope_fn = GetUpvalue(start_fn, path)
+    if not fn then print("Didn't find "..path.." from", start_fn) return end
+    debug.setupvalue(scope_fn, fn_i, new_fn)
 end
- 
+
 return {
     GetUpvalue = GetUpvalue,
     SetUpvalue = SetUpvalue
