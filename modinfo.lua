19bc837d3f0a36a35082298209ef10e1edb333b7
秋一 <civi@rcg.fun>
2021-08-11 23:37:55 +0800

    update to 2.4, fix translate issue.

--- modinfo.lua
+++ modinfo.lua
@@ -1,13 +1,11 @@
-version = "2.3"
+version = "2.4"
 name = "Glassic API"
 author = "Civi, Tony, LSSSS"
 description = locale == "zh" and
 "[版本: "..version..[[]
 
 更新内容:
-- 更新 UpvalueHacker.
-
-- 皮肤组件现在可以自动判断基础实体名是否是人物.
+- 修复翻译模块.
 
 包含了皮肤组件和一套玻璃工具.
 ]]
@@ -15,9 +13,7 @@ or
 "[Version: "..version..[[]
 
 Changelog: 
-- Update UpvalueHacker.
-
-- Update SkinHandler. Characters no longer need "is_char = true".
+- Fixed translate function.
 
 Included Skin Handler API and Moon Glass Tools / Weapon.
 ]]
