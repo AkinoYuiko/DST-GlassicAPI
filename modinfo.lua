27cf5cf7cb672eca004674ee3460a8e23402368a
秋一 <civi@rcg.fun>
2021-12-24 02:31:01 +0800

    Update to 2.9.6

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,13 @@
-version = "2.9.5"
+version = "2.9.6"
 name = "Glassic API"
 author = "Civi, Tony, LSSSS"
 description = locale == "zh" and
 "[版本: "..version..[[]
 
 更新内容:
-- 修复换皮肤逻辑和漂浮动画的问题.
+- 调整SkinHandler中的一些代码格式.
 
+- 修复换皮肤逻辑和漂浮动画的问题.
 - 调整Tuning.
 - 修复SkinHandler中关于ReskinEntity的一些逻辑问题.
 - 修复LanguageTranslator.
@@ -21,8 +22,9 @@ or
 "[Version: "..version..[[]
 
 Changelog:
-- Fix issue for reskin logic and floating anim.
+- Tweak code format in SkinHandler.
 
+- Fix issue for reskin logic and floating anim.
 - Tweak Tuning.
 - Fix logic issue for "ReskinEntity" in SkinHandler.
 - Fix LanguageTranslator.
