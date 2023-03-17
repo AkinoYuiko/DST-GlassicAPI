3a12b2cc34bf3652c55efa58824263eb5308c1d1
Civi <civi@rcg.fun>
2023-03-18 06:18:15 +0800

    Increase version to 4.0

--- modinfo.lua
+++ modinfo.lua
@@ -10,27 +10,17 @@ local function zh_en(a, b)
     })
 end
 
-version = "3.4.5"
+version = "4.0"
 name = "Glassic API"
 author = "Civi, Tony, U.N. Owen, LSSSS"
 changelog = zh_en([[
-- 调整了AddRecipe示例中的代码。
-
-- 优化了一处代码逻辑。
-- 为玻璃魔杖皮肤新增特殊文本。
-- 新增懒人魔杖皮肤：玻璃魔杖。
-- 升级了 GlassicAPI.BasicInitFn。
+- 本API不再包含示例道具和皮肤，如需学习请参考【暗夜故事集】里面的写法
 ]], [[
-- Tweak code logic for example recipe description.
-
-- Tweak code logic for hidden recipes in searching.
-- Add special text string for skin: Rod of Glass.
-- Add skin: Rod Of Glass (Lazy Explorer).
-- Make GlassicAPI.BasicInitFn better for use.
+- Migrate example items and skins to "Night Stories" mod.
 ]])
 description = zh_en("版本: ", "Version: ") .. version ..
     zh_en("\n\n更新内容:\n", "\n\nChangelog:\n") .. changelog .. "\n" ..
-    zh_en("“帮助你快速创建一个模组以及制作皮肤，同时还包含了一套玻璃工具和武器。”", "\"helps create mod and mod skins quickly, and includes extra Moon Glass tools and weapons.\"")
+    zh_en("“帮助你快速创建一个模组以及制作皮肤。”", "\"helps create mod and mod skins quickly.\"")
 
 priority = 2147483647
 
